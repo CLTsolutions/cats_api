a2fe5c7708078d610e7b007477371ed14cd88359
Chelsey <chelsey.tschida@outlook.com>
2021-04-04 15:51:01 -0400

    updated async with try/catch / minor updates to styling

--- main.js
+++ main.js
@@ -5,21 +5,25 @@ const section = document.querySelector('div')
 const button = document.querySelector('.btn')
 
 // handling errs differently than try/catch
-getRandomCats().catch(error => {
-    console.log('This is an error.')
-    console.log(error)
-})
+// async would need to be refactored as 'async function getRandomCats()'
+// getRandomCats().catch(error => {
+//     console.log('This is an error.')
+//     console.log(error)
+// })
 
 // fetching from the api
-async function getRandomCats() {
+getRandomCats = async () => {
     section.innerHTML = ''
 
-    const response = await fetch(baseUrl)
-    console.log("Response:", response)
-    const json = await response.json()
-    console.log('JSON:', json)
-    console.log(json[0].url)
-    return randomCatPhoto(json)
+    try {
+        const response = await fetch(baseUrl)
+        const json = await response.json()
+        console.log('JSON:', json)
+        return randomCatPhoto(json)
+    } catch (e) {
+        console.log('This is an error');
+        console.log(e);
+    }
 }
 
 randomCatPhoto = json => {
